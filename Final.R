f5437b18697db8c7e3b76818bd2de76d0b37716e
ecm5846 <ecm5846@psu.edu>
2026-03-17 23:02:22 -0400

    Test - first plot.

--- Final.R
+++ Final.R
@@ -49,9 +49,9 @@ df_filtered <- function(df) {
       arrange(desc(total))
   }
 
-trone_fin_dist <- summarize_finances(trone_data)
-delaney_fin_dist <- summarize_finances(delaney_data)
-thompson_fin_dist <- summarize_finances(thompson_data)
+trone_fin_dist <- summarize_finances(trone_data) %>% mutate(candidate = "Trone")
+delaney_fin_dist <- summarize_finances(delaney_data) %>% mutate(candidate = "Delaney")
+thompson_fin_dist <- summarize_finances(thompson_data) %>% mutate(candidate = "Thompson")
 
 ## Find top 5 donors
 summarrize_donors <- function(df) {
@@ -61,9 +61,37 @@ summarrize_donors <- function(df) {
     slice_max(total, n = 10) %>% 
     arrange(desc(total))
 }
-trone_donor_dist <- summarrize_donors(trone_data)
-delaney_donor_dist <- summarrize_donors(delaney_data)
-thompson_donor_dist <- summarrize_donors(thompson_data)
+trone_donor_dist <- summarrize_donors(trone_data) %>% mutate(candidate = "Trone")
+delaney_donor_dist <- summarrize_donors(delaney_data) %>% mutate(candidate = "Delaney")
+thompson_donor_dist <- summarrize_donors(thompson_data) %>% mutate(candidate = "Thompson")
 
+## Combine all data
 all_donors <- bind_rows(trone_donor_dist, delaney_donor_dist, thompson_donor_dist)
+all_dist <- bind_rows(trone_fin_dist, delaney_fin_dist, thompson_fin_dist)
+
+all_dist %>% 
+  filter(!entity_type_desc %in% c("CAMPAIGN COMMITTEE", "POLITICAL PARTY COMMITTEE", "OTHER COMMITTEE")) %>%
+  
+  ggplot(aes(x = entity_type_desc, y = total, fill = candidate)) +
+  geom_col(position = "dodge") +
+  
+  geom_text(
+    aes(label = scales::comma(total)),
+    position = position_dodge(width = 0.9),
+    vjust = -0.3,
+    size = 3
+  ) +
+  
+  theme(
+    panel.grid.minor = element_blank(),
+    plot.margin = margin(6, 8, 6, 8),
+    # axis.text.x = element_text(angle = 40, hjust = 1, vjust = 1),
+    legend.position = "bottom"
+  ) +
+  
+  labs(
+    title = "Contribution Totals by Entity Type",
+    x = NULL,
+    y = "Total Contributions"
+  )
 
